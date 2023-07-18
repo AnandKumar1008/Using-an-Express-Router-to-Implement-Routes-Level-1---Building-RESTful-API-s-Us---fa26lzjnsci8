59c5380120b7179fb3a767efab6ae5d87f3c031c
AnandKumar <anandkumar031199@gmail.com>
2023-07-18 14:12:13 +0530

    changes are done

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
-const fs = require('fs');
-const express = require('express');
+const fs = require("fs");
+const express = require("express");
 const app = express();
+const router = new express.Router();
 //Aim: With the help of router, get all the product with router.GET request and create a product with router.POST request
 
 //middleware
 //write router middleware here
+app.use(express.json());
+app.use(router);
 
 //Including product.json file
 const product = JSON.parse(
@@ -13,18 +16,55 @@ const product = JSON.parse(
 
 // Defining The Router
 // Get all the products
-router.get('/api/v1/product', (req, res) => {
+router.get("/api/v1/product", (req, res) => {
   try {
     //Write your code here
+    // console.log(product);
+    // console.log("request coming");
+    res.status(200).json({
+      status: "success",
+
+      results: 10,
+
+      data: {
+        product: [...product],
+      },
+    });
   } catch (error) {
     res.status(400).json(error);
   }
 });
 
 //Create a new Product
-router.post('/api/v1/product', (req, res) => {
+router.post("/api/v1/product", (req, res) => {
   try {
     //Write your code here
+    if (!req.body.title || !req.body.price) {
+      res.status(404).json({
+        message: "Title and price are required",
+
+        status: "Error",
+      });
+    } else {
+      const toAdd = req.body;
+      toAdd.id = product.length + 1;
+      product.push({ ...toAdd });
+      fs.writeFile(
+        `${__dirname}/../dev-data/product.json`,
+        JSON.stringify(product),
+        (err) => {
+          res.json({
+            status: "Success",
+
+            data: {
+              product: {
+                ...toAdd,
+              },
+            },
+          });
+        }
+      );
+    }
   } catch (error) {
     res.status(400).json(error);
   }
